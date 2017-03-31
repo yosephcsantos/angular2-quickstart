10594fad2b0e56a84bfc0a562cc4dcadc013f7e2
YosephCode <yosephcsantos@gmail.com>
2017-03-31 20:00:32 -0300

    crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Account} from './account.model';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html',
-  styles: ['.blue-bg{background-color:blue;}']
+  templateUrl: 'app/app.component.html'
 })
 
-export class AppComponent  {
-  private username:string = 'Yoseph'
-  private currentUser:User = { username: 'Jobson', email: 'job@son.com' }
-
-  private hideEmail:boolean = false
-  private disabledInput:boolean = true
-  private isBlueBg:boolean= true
-  private bgColor:string = 'red'
-
-  private getUsername():string {
-    return this.currentUser.username
-  }
-
-  private isDisabled():boolean {
-    return this.disabledInput
-  }
-
-  private toogleLeave():void {
-    this.bgColor = 'yellow'
-  }
-
-  private toogle():void {
-    this.bgColor = 'green'
-  }
-
-  private toogleEmail() {
-    this.hideEmail = !this.hideEmail
-  }
-
-  private changeEmail(newMail:string) {
-    this.currentUser.email = newMail
-  }
-
-  private keyPress(event:KeyboardEvent) {
-    if(event.keyCode == 13) {
-      var inputElem = <HTMLInputElement>event.target
-      this.changeEmail(inputElem.value)
-    }
-  }
-
-  private users:Array<User> = [
-    {username: 'one', email: '1@son.com'},
-    {username: 'two', email: '2@son.com'},
-    {username: 'three', email: '3@son.com'},
-    {username: 'four', email: '4@son.com'}
+export class AppComponent {
+  private _accounts:Array<Account> = [
+    { id:1, title:'Bank Pactual', description:'this is my main bank', balance:5 },
+    new Account( 2, 'Bank ixda', 'secret bank', 2000)
   ]
-}
 
-interface User {
-  username: string,
-  email:string
+  private _nextId = 3
+
+  private resetDataBank(titleEl:any, descEl:any, balEl:any) {
+    titleEl.value = ''
+    descEl.value = ''
+    balEl.value = 0
+  }
+
+  private createAcc(titleEl:any, descEl:any, balEl:any) {
+    this._accounts.push(new Account(this._nextId, titleEl.value, descEl.value, balEl.value))
+    this._nextId++
+
+    this.resetDataBank(titleEl, descEl, balEl);
+  }
+
+  private removeAcc(index:number) {
+    this._accounts.splice(index, 1)
+  }
 }
